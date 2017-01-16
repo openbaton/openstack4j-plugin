49b472908ea9aed26d484ee2489fc2dd9e4b02cc
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-01-16 16:27:07 +0100

    fix: find vm in error state

--- src/main/java/org/openbaton/drivers/openstack4j/OpenStack4JDriver.java
+++ src/main/java/org/openbaton/drivers/openstack4j/OpenStack4JDriver.java
@@ -18,18 +18,7 @@ package org.openbaton.drivers.openstack4j;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+
 import org.apache.commons.codec.binary.Base64;
 import org.openbaton.catalogue.mano.common.DeploymentFlavour;
 import org.openbaton.catalogue.nfvo.NFVImage;
@@ -64,6 +53,19 @@ import org.openstack4j.model.network.RouterInterface;
 import org.openstack4j.openstack.OSFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /** Created by gca on 10/01/17. */
 public class OpenStack4JDriver extends VimDriver {
@@ -432,7 +434,7 @@ public class OpenStack4JDriver extends VimDriver {
           log.debug("Finished deployment of VM with hostname: " + name);
           bootCompleted = true;
         }
-        if (server.getStatus().equals("ERROR")) {
+        if (server.getExtendedStatus().equalsIgnoreCase("ERROR") || server.getStatus().equalsIgnoreCase("ERROR")) {
           log.error("Failed to launch VM with hostname: " + name + " -> Went into ERROR");
           VimDriverException vimDriverException =
               new VimDriverException(server.getExtendedStatus());
